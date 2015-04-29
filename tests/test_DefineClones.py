86827d0ba8705a45ddf71a7dff05402d99eb59a9
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-04-29 18:06:27 -0400

    1. Fixed errors in DefineClones unit tests. 2. Updated igblast test cases.

--- tests/test_DefineClones.py
+++ tests/test_DefineClones.py
@@ -6,19 +6,20 @@ __author__    = 'Jason Anthony Vander Heiden'
 __copyright__ = 'Copyright 2014 Kleinstein Lab, Yale University. All rights reserved.'
 __license__   = 'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported'
 __version__   = '0.4.0'
-__date__      = '2015.01.28'
+__date__      = '2015.04.29'
 
 # Imports
 import time, unittest, os
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from IgCore import getScoreDict
-from DbCore import IgRecord, readDbFile
+from DbCore import IgRecord, readDbFile, getDistMat
 import DefineClones as mod
-from pandas import read_csv
+import pandas as pd
 
 # Globals
 data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'models')
 
 class Test_DefineClones(unittest.TestCase):
     def setUp(self):
@@ -74,44 +75,50 @@ class Test_DefineClones(unittest.TestCase):
         # prof.dump_stats('hs5f-unit-test-dict.prof')
 
         # Define data files
-        model_file = os.path.join(data_path, 'HS5F_Targeting.tab')
-        dist_dict = read_csv(model_file, sep='\t', index_col=0).to_dict()
+        model_file = os.path.join(model_path, 'HS5F_Targeting.tab')
+        dist_dict = pd.read_csv(model_file, sep='\t', index_col=0).to_dict()
 
         results = mod.distanceClones(self.records, model='hs5f', distance=1.0, dist_mat=dist_dict)
+        results = {k: sorted(v, key=lambda x: x.id) for k, v in results.iteritems()}
         print 'MODEL> hs5f'
         for k, v in results.iteritems():
             for s in v:
                 print '  CLONE-%i> %s' % (k, s.id)
 
-        self.assertEqual(results, self.clones)
+        self.assertEqual(sorted(self.clones.values()), sorted(results.values()))
 
         # Define data files
-        model_file = os.path.join(data_path, 'M3N_Targeting.tab')
-        dist_dict = read_csv(model_file, sep='\t', index_col=0).to_dict()
+        model_file = os.path.join(model_path, 'M3N_Targeting.tab')
+        dist_dict = pd.read_csv(model_file, sep='\t', index_col=0).to_dict()
 
         results = mod.distanceClones(self.records, model='m3n', distance=1.0, dist_mat=dist_dict)
+        results = {k: sorted(v, key=lambda x: x.id) for k, v in results.iteritems()}
         print 'MODEL> m3n'
         for k, v in results.iteritems():
             for s in v:
                 print '  CLONE-%i> %s' % (k, s.id)
 
-        self.assertEqual(results, self.clones)
+        self.assertEqual(sorted(self.clones.values()), sorted(results.values()))
 
-        results = mod.distanceClones(self.records, model='aa', distance=0.0)
+        smith96 = pd.DataFrame([[0,2.86,1,2.14],[2.86,0,2.14,1],[1,2.14,0,2.86],[2.14,1,2.86,0]],
+                                index=['A','C','G','T'], columns=['A','C','G','T'], dtype=float)
+        dist_dict = getDistMat(smith96)
+        results = mod.distanceClones(self.records, model='m1n', distance=10, dist_mat=dist_dict)
+        results = {k: sorted(v, key=lambda x: x.id) for k, v in results.iteritems()}
+        print 'MODEL> m1n'
+        for k, v in results.iteritems():
+            for s in v:
+                print '  CLONE-%i> %s' % (k, s.id)
+        self.assertEqual(sorted(self.clones.values()), sorted(results.values()))
+
+        results = mod.distanceClones(self.records, model='aa', distance=3.0)
+        results = {k: sorted(v, key=lambda x: x.id) for k, v in results.iteritems()}
         print 'MODEL> aa'
         for k, v in results.iteritems():
             for s in v:
                 print '  CLONE-%i> %s' % (k, s.id)
 
-        self.assertEqual(results, self.clones)
-
-        results = mod.distanceClones(self.records, model='m1n', distance=10.0)
-        print 'MODEL> m1n'
-        for k, v in results.iteritems():
-            for s in v:
-                print '  CLONE-%i> %s' % (k, s.id)
-
-        self.assertEqual(results, self.clones)
+        self.assertEqual(sorted(self.clones.values()), sorted(results.values()))
 
 
 if __name__ == '__main__':
