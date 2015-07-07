ed85b997bbcd281fbd219a3cf0ff6fe77860792d
namita1025 <namita.gupta@yale.edu>
2015-07-07 16:44:07 -0400

    Updated SEQUENCE_VDJ for MakeDb igblast to use igblast aligned query sequence, accounting for gaps in the alignment.

--- tests/test_MakeDb.py
+++ tests/test_MakeDb.py
@@ -25,8 +25,19 @@ class Test_MakeDb(unittest.TestCase):
         print '-> %s()' % self._testMethodName
 
         # Define data files
-        self.igblast_fmt7_file = os.path.join(data_path, 'igblast_test.fmt7')
-        self.igblast_seq_dict = MakeDb.getSeqforIgBlast(os.path.join(data_path, 'igblast_test.fasta'))
+        # Created by: ./igblastn -germline_db_V database/IMGT_Human_IGHV -germline_db_D database/IMGT_Human_IGHD
+        # -germline_db_J database/IMGT_Human_IGHJ -domain_system imgt -ig_seqtype Ig -outfmt '7 std qseq'
+        # -auxiliary_data optional_file/human_gl.aux -query ~/workspace/changeo/tests/data/igblast_test_ig.fasta
+        # -out ~/workspace/changeo/tests/data/igblast_test_ig.fmt7
+        self.igblast_ig_fmt7_file = os.path.join(data_path, 'igblast_test_ig.fmt7')
+        self.igblast_ig_seq_dict = MakeDb.getSeqforIgBlast(os.path.join(data_path, 'igblast_test_ig.fasta'))
+
+        # Created by: ./igblastn -germline_db_V database/imgt_human_tr_v -germline_db_D database/imgt_human_tr_d
+        # -germline_db_J database/imgt_human_tr_j -domain_system imgt -ig_seqtype TCR -outfmt '7 std qseq'
+        # -auxiliary_data optional_file/human_gl.aux -query ~/workspace/changeo/tests/data/igblast_test_tr.fasta
+        # -out ~/workspace/changeo/tests/data/igblast_test_tr.fmt
+        self.igblast_tr_fmt7_file = os.path.join(data_path, 'igblast_test_tr.fmt7')
+        self.igblast_tr_seq_dict = MakeDb.getSeqforIgBlast(os.path.join(data_path, 'igblast_test_tr.fasta'))
 
         self.start = time.time()
 
@@ -36,13 +47,24 @@ class Test_MakeDb(unittest.TestCase):
 
     #@unittest.skip("-> readIgBlast() skipped\n")
     def test_readIgBlast(self):
-        result = MakeDb.readIgBlast(self.igblast_fmt7_file, self.igblast_seq_dict)
+
+        print 'Testing IG\n'
+        result = MakeDb.readIgBlast(self.igblast_ig_fmt7_file, self.igblast_ig_seq_dict)
         for x in result:
             print '   ID> %s' % x.id
             print 'VCALL> %s' % x.v_call
             print 'INPUT> %s' % x.seq_input
             print '  VDJ> %s' % x.seq_vdj
-            print ' JUNC> %s' % x.junction
+            print ' JUNC> %s\n' % x.junction
+
+        print 'Testing TCR\n'
+        result = MakeDb.readIgBlast(self.igblast_tr_fmt7_file, self.igblast_tr_seq_dict)
+        for x in result:
+            print '   ID> %s' % x.id
+            print 'VCALL> %s' % x.v_call
+            print 'INPUT> %s' % x.seq_input
+            print '  VDJ> %s' % x.seq_vdj
+            print ' JUNC> %s\n' % x.junction
 
         self.fail()
 
