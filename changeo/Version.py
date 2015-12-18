a77cc73cc5181a928bc24dce1ed46b76893b3346
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-12-18 14:31:40 -0500

    Bumped version and date.

--- changeo/Version.py
+++ changeo/Version.py
@@ -4,5 +4,5 @@ Version and authorship information
 __author__    = 'Namita Gupta'
 __copyright__ = 'Copyright 2013 Kleinstein Lab, Yale University. All rights reserved.'
 __license__   = 'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported'
-__version__   = '0.3.0.999'
-__date__      = '2015.12.7'
+__version__   = '0.3.1'
+__date__      = '2015.12.18'
