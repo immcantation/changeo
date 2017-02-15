cc91ea280365b3d02ad4a4490d26f247c7150826
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-02-15 17:10:34 -0500

    nevermind.

--- docs/conf.py
+++ docs/conf.py
@@ -22,19 +22,14 @@ from unittest.mock import MagicMock
 import changeo.Version
 
 # Mock modules for readthedocs
-# TODO:  I think we can removed the presto mocks later - once a version of presto with the rtd check is on PyPI
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):  return Mock()
-
 mock_modules = ['numpy', 'scipy', 'scipy.cluster.hierarchy', 'scipy.spatial.distance',
-                 'pandas', 'Bio', 'Bio.Align', 'Bio.Align.Applications', 'Bio.Alphabet',
-                 'Bio.Seq', 'Bio.SeqRecord']
-# mock_modules = ['numpy', 'scipy', 'scipy.cluster.hierarchy', 'scipy.spatial.distance',
-#                 'pandas', 'Bio', 'Bio.Align', 'Bio.Align.Applications', 'Bio.Alphabet',
-#                 'Bio.Seq', 'Bio.SeqRecord',
-#                 'presto', 'presto.Annotation', 'presto.Applications', 'presto.Defaults',
-#                 'presto.IO', 'presto.Multiprocessing', 'presto.Sequence']
+                'pandas', 'Bio', 'Bio.Align', 'Bio.Align.Applications', 'Bio.Alphabet',
+                'Bio.Seq', 'Bio.SeqRecord',
+                'presto', 'presto.Annotation', 'presto.Applications', 'presto.Defaults',
+                'presto.IO', 'presto.Multiprocessing', 'presto.Sequence']
 sys.modules.update((mod_name, Mock()) for mod_name in mock_modules)
 
 # If extensions (or modules to document with autodoc) are in another directory,
