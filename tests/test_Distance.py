6ef592bd94a4309242f86093f6d638695f80baea
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-03-15 19:03:25 -0400

    Added some incomplete unit test stuff for `changeo.Distance.calcDistances`.

--- tests/test_Distance.py
+++ tests/test_Distance.py
@@ -9,6 +9,7 @@ from changeo import __version__, __date__
 import os
 import time
 import unittest
+from Bio.Seq import Seq
 
 # Presto and changeo imports
 import changeo.Distance as Distance
@@ -21,9 +22,15 @@ class Test_Distance(unittest.TestCase):
     def setUp(self):
         print('-> %s()' % self._testMethodName)
         
-        self.seq_list = ['TGTGCAAGGGGGCCATTGGACTACTTCTACTACGGTGTGGACGTCTGG',
-                         'TGTAGGGGGCCATTGGACTACTTCTACTACGGTGTGGACGTCTGG']
-                         
+        self.seq_error = ['TGTGCAAGGGGGCCATTGGACTACTTCTACTACGGTGTGGACGTCTGG',
+                          'TGTAGGGGGCCATTGGACTACTTCTACTACGGTGTGGACGTCTGG']
+        self.seq_valid = ['TGTGCAAGGGGGCCA',
+                          'TGTGCAAGGGGGCCA',
+                          'TGTATTTGGGGGCCA',
+                          'ACACGTTCCCCCGGT',
+                          'NNNNNNNNNNNNNNN']
+        self.aa_valid = [str(Seq(x).translate()) for x in self.seq_valid]
+
         self.start = time.time()
 
     def tearDown(self):
@@ -31,9 +38,31 @@ class Test_Distance(unittest.TestCase):
         print("<- %s() %.3f" % (self._testMethodName, t))
         
     def test_calcDistances(self):
+        # aa
+        result = Distance.calcDistances(self.aa_valid, n=1, dist_mat=Distance.aa_model,
+                                        norm='len', sym='min')
+        print('    AA>\n', result)
+
+        # ham
+        result = Distance.calcDistances(self.seq_valid, n=1, dist_mat=Distance.ham_model,
+                                        norm='len', sym='min')
+        print('   HAM>\n', result)
+
+        # hh_s1f
+        result = Distance.calcDistances(self.seq_valid, n=1, dist_mat=Distance.hh_s1f_model,
+                                        norm='len', sym='min')
+        print('HH_S1F>\n', result)
+
+        # hh_s5f
+        result = Distance.calcDistances(self.seq_valid, n=5, dist_mat=Distance.hh_s5f_model,
+                                        norm='len', sym='min')
+        print('HH_S5F>\n', result)
+
+        # Raise error on length mismatch
         with self.assertRaises(IndexError):
-	        Distance.calcDistances(self.seq_list, n = 1, dist_mat = Distance.hh_s1f_model,
-                                   norm = 'len', sym = 'min')
+            Distance.calcDistances(self.seq_error, n=1, dist_mat=Distance.hh_s1f_model,
+                                   norm='len', sym='min')
+
 
     @unittest.skip("-> loadModels() skipped\n")
     def test_loadModels(self):
