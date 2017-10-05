154f48333a29271b46aa039567ab4e9b7892af8c
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-10-05 16:16:20 -0400

    Minor.

--- docs/examples/scripts/IgBLAST_Wrapper.sh
+++ docs/examples/scripts/IgBLAST_Wrapper.sh
@@ -5,4 +5,4 @@ fetch_imgtdb.sh -o ~/share/germlines/imgt
 # Build IgBLAST database from IMGT reference sequences
 imgt2igblast.sh -i ~/share/germlines/imgt -o ~/share/igblast
 # Run IgBLAST for human B cell data
-run_igblast.sh -s reads.fasta -g human -t ig -b ~/share/igblast -n 2
+run_igblast.sh -s reads.fasta -g human -t ig -b ~/share/igblast
